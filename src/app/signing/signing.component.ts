19bb8ac5d5aecd57f02a6397e3c224f22c709291
bogdan <bohdan549@gmail.com>
2017-09-11 23:13:14 +0300

    FE-0002 added signIn/signUp form

--- src/app/signing/signing.component.ts
+++ src/app/signing/signing.component.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'signing',
   templateUrl: './signing.component.html',
+  styleUrls : ['./signinig.component.css']
 })
 export class SigningComponent implements OnInit {
   constructor() {}
